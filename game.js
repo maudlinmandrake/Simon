a2705ea417f8a003528e2476cdabd5249742c22f
Jennifer Mikac <jennymikac@gmail.com>
2020-07-04 14:16:50 -0400

    Refactor for playSound function
    Creating function playSound() and refactoring code so function will trigger upon a new sequence generated OR user mouse click detected.

--- game.js
+++ game.js
@@ -1,8 +1,22 @@
 
 var buttonColors = ["red", "blue", "green", "yellow"];
 var gamePattern = [];
+var userClickedPattern = [];
 
-//Generate a random color and create a sequence of colors
+//Detect users clicks and push to an array for storage
+$(".btn").click(function() {
+
+    var userChosenColor = $(this).attr("id");
+    userClickedPattern.push(userChosenColor);
+
+});
+
+function playSound(name) {
+
+    var audio = new Audio("sounds/" + randomChosenColor + ".mp3");
+    audio.play();
+}
+//Generate new sequence
 function newSequence() {
 
     var randomNumber = Math.floor(Math.random() * 4);
@@ -12,6 +26,6 @@ function newSequence() {
 
     $("#" + randomChosenColor).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
 
-    var audio = new Audio("sounds/" + randomChosenColor + ".mp3");
-    audio.play();
+    playSound(randomChosenColor);
+
 }
