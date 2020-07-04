c2a92079be0395c63466d693f51209c196e76d06
Jennifer Mikac <jennymikac@gmail.com>
2020-07-04 13:01:26 -0400

    newSequence function
    Function will now chose a random color and then make the corresponding button blink, to indicate a new color in the pattern.

--- game.js
+++ game.js
@@ -1,5 +1,14 @@
 
-function nextSequence() {
+var buttonColors = ["red", "blue", "green", "yellow"];
+var gamePattern = [];
+
+//Generate a random color and create a sequence of colors
+function newSequence() {
+
     var randomNumber = Math.floor(Math.random() * 4);
-    console.log(randomNumber);
+    var randomChosenColor = buttonColors[randomNumber];
+
+    gamePattern.push(randomChosenColor);
+
+    $("#" + randomChosenColor).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
 }
