efa86e7446f4981af2e7a07f63d5e1e7f8fa03bd
Jennifer Mikac <jennymikac@gmail.com>
2020-07-04 15:11:02 -0400

    Start a new game

--- game.js
+++ game.js
@@ -2,25 +2,48 @@
 var buttonColors = ["red", "blue", "green", "yellow"];
 var gamePattern = [];
 var userClickedPattern = [];
+var started = false;
+var level = 0;
+
+//Start a new game
+$(document).keypress(function() {
+     if (started === false) {
+       $("h1").text("Level " + level);
+       newSequence();
+       started = true;
+     }
+});
+
 
 //Play sound upon user button click or new color added to sequence by newSequence()
 function playSound(name) {
 
     var audio = new Audio("sounds/" + name + ".mp3");
     audio.play();
+
 }
 
-//Detect users clicks and push to an array for storage
+//Animates user button click
+function animatePress(currentColor) {
+
+        $("#" + currentColor).addClass("pressed");
+
+        setTimeout(function() {
+            $("#" + currentColor).removeClass("pressed");
+        }, 100)
+}
+
+//Detects and adds additional behaviors upon user mouse click
 $(".btn").click(function() {
 
     var userChosenColor = $(this).attr("id");
     userClickedPattern.push(userChosenColor);
     playSound(userChosenColor);
-
+    animatePress(userChosenColor);
 });
 
 
-//Generate new sequence
+//Generate a new color sequence pattern for user to follow
 function newSequence() {
 
     var randomNumber = Math.floor(Math.random() * 4);
