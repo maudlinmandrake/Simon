59f2b3397ca8f32c009efbb4a97309fb6f17dedf
Jennifer Mikac <jennymikac@gmail.com>
2020-07-05 12:02:09 -0400

    Added start over function
    Added function to restart game if user choses wrong color in sequence

--- game.js
+++ game.js
@@ -17,6 +17,75 @@ $(document).keypress(function() {
      }
 });
 
+// user input -------------------------
+
+$(".btn").click(function() {
+
+    var userChosenColor = $(this).attr("id");
+    userClickedPattern.push(userChosenColor);
+    playSound(userChosenColor);
+    animatePress(userChosenColor);
+    checkAnswer(userClickedPattern.length - 1);
+});
+
+// check user answer
+
+function checkAnswer(currentLevel) {
+
+    if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
+
+        console.log("success")
+
+        if (gamePattern.length === userClickedPattern.length) {
+           
+            setTimeout(function() {
+                newSequence();
+              }, 1000);
+
+        }
+    } else {
+
+        $("h1").text("Game Over, Press Any Key to Restart");
+        $("body").addClass("game-over");
+
+        setTimeout(function(){
+            $("body").removeClass("game-over");
+        }, 200)
+
+        var wrong = new Audio("sounds/wrong.mp3");
+        wrong.play();
+
+        startOver();
+    }
+}
+
+// generate new sequence ------------------
+
+function newSequence() {
+ 
+    userClickedPattern = [];
+
+    level++;
+    $("h1").text("Level " + level);
+
+    var randomNumber = Math.floor(Math.random() * 4);
+    var randomChosenColor = buttonColors[randomNumber];
+    gamePattern.push(randomChosenColor);
+
+    $("#" + randomChosenColor).fadeIn(100).fadeOut(100).fadeIn(100);
+    playSound(randomChosenColor);
+
+}
+
+// start the game over --------------------
+
+function startOver() {
+
+    level = 0;
+    gamePattern = [];
+    started = false;
+
+}
 
 // play sound ----------------------------
 
@@ -36,55 +105,4 @@ function animatePress(currentColor) {
         setTimeout(function() {
             $("#" + currentColor).removeClass("pressed");
         }, 100)
-}
-
-// user input -------------------------
-
-$(".btn").click(function() {
-
-    var userChosenColor = $(this).attr("id");
-    userClickedPattern.push(userChosenColor);
-    playSound(userChosenColor);
-    animatePress(userChosenColor);
-    checkAnswer(userClickedPattern.length - 1);
-});
-
-// check user answer
-
-function checkAnswer(currentLevel) {
-
-    if (userClickedPattern[currentLevel] === gamePattern[currentLevel]) {
-        if (userClickedPattern.length === gamePattern.length) {
-            setTimeout(function() {
-                newSequence();
-              }, 1000);
-        }
-    } else {
-        console.log("wrong")
-    }
-}
-
-// generate new sequence ------------------
-
-function newSequence() {
-
-    // reset user answers upon new sequence    
-    userClickedPattern = [];
-
-    // increase level upon creating new sequence
-    level++;
-    $("h1").text("Level " + level);
-
-    // generate a new color for the pattern
-    var randomNumber = Math.floor(Math.random() * 4);
-    var randomChosenColor = buttonColors[randomNumber];
-
-    // add new color to the game sequence to create pattern
-    gamePattern.push(randomChosenColor);
-
-    //animation to visually indicate pattern
-    $("#" + randomChosenColor).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
-
-    playSound(randomChosenColor);
-
 }
